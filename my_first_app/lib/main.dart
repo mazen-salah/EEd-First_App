0ce0acb40b61af463d663b2866d8f5bcbd88e306
mazen <es-mazensalah2025@alexu.edu.eg>
2023-02-15 02:12:44 +0200

    new session

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_app/screens/home.dart';
+import 'screens/home.dart';
 
 void main() {
   runApp(const MyApp());
--- my_first_app/lib/screens/home.dart
+++ my_first_app/lib/screens/home.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_app/customs.dart';
 
-class HomePage extends StatelessWidget {
+bool passwordVisibility = false;
+TextEditingController emailController = TextEditingController();
+TextEditingController passwordController = TextEditingController();
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -23,23 +32,151 @@ class HomePage extends StatelessWidget {
                 borderRadius: BorderRadius.all(Radius.circular(40.0))),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              // crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                // const Icon(
-                //   Icons.person,
-                // ),
-                Text(
-                  "Login",
+                const Text(
+                  "Sign in",
                   style: TextStyle(
                     fontSize: 30,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
+                Column(
+                  children: [
+                    myCustomText(
+                        label: "Email",
+                        hint: "Enter your email",
+                        icon: Icons.email,
+                        controller: emailController),
+                    myCustomText(
+                        label: "Password",
+                        hint: "Enter your password",
+                        icon: Icons.lock,
+                        controller: passwordController),
+                  ],
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(200, 50),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (emailController.text == 'admin' &&
+                        passwordController.text == 'admin') {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginSuccess()));
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text("Login Successful"),
+                              content: const Text("Welcome Admin"),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text("OK"))
+                              ],
+                            );
+                          });
+                    } else {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text("Login Unsuccessful"),
+                              content: const Text("Please, try again"),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text("OK"))
+                              ],
+                            );
+                          });
+                    }
+                  },
+                  child: const Text("Sign in", style: TextStyle(fontSize: 20)),
+                ),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Padding myCustomText({
+    required String label,
+    required String hint,
+    required IconData icon,
+    required TextEditingController controller,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, bottom: 8),
+            child: Text("$label :",
+                style: const TextStyle(color: Colors.white, fontSize: 20)),
+          ),
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(40.0)),
+            ),
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              onSubmitted: (value) {},
+              obscureText: label == "Password" ? passwordVisibility : false,
+              controller: controller,
+              decoration: InputDecoration(
+                suffix: label == 'Password'
+                    ? IconButton(
+                        onPressed: () {
+                          setState(() {
+                            passwordVisibility = !passwordVisibility;
+                          });
+                        },
+                        icon: Icon(passwordVisibility
+                            ? Icons.visibility
+                            : Icons.visibility_off))
+                    : null,
+                prefixIcon: Icon(icon),
+                label: Text(label),
+                border: InputBorder.none,
+                hintText: hint,
+                hintStyle: const TextStyle(
+                  color: Colors.black38,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LoginSuccess extends StatelessWidget {
+  const LoginSuccess({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Success"),
+      ),
+      body: const Center(
+        child: Text("Welcome Admin"),
       ),
     );
   }
